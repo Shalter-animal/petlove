e0a9c4cfdbc324b2442cd456e1d1de4e5cb990b4
Andrii Yanchuk <horeca288@gmail.com>
2025-10-28 18:43:02 +0200

    remove loading

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -9,19 +9,15 @@ import { MainPagination } from "@/components/MainPagination";
 export default function News() {
   const [searchItem, setSearchItem] = useState("");
   const [news, setNewsData] = useState<NewsResponse | null>(null);
-  const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
-        setLoading(true);
         const data = await getNews(currentPage, searchItem);
         setNewsData(data);
       } catch (error) {
         console.error(error);
-      } finally {
-        setLoading(false);
       }
     };
     fetchNews();
@@ -44,8 +40,6 @@ export default function News() {
     <>
       <Header />
       <main className="pt-16 px-5">
-        {loading && <p>Loading...</p>}
-
         <div className="flex flex-col md:flex-row md:items-center md:justify-between">
           <h1 className="font-bold text-[28px] leading-[28px] h-fit md:text-[54px] md:leading-[54px]">
             News
